53cb296b5e290f2b063637b22e9cd1568d3f9032
flasheur111 <francois.boiteux@epita.fr>
2014-03-17 23:34:29 +0100

    Fix name winform

--- 2048.cs
+++ 2048.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace _2048AI
 {
-    public partial class Form1 : Form
+    public partial class _2048 : Form
     {
-        public Form1()
+        public _2048()
         {
             InitializeComponent();
         }
