040f8ce37cdf03da42a957e06caf4b99d31f89c4
flasheur111 <francois.boiteux@epita.fr>
2014-03-24 00:29:40 +0100

    End merge board

---
+++ 2048/2048/Board.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+
+    public class Board
+    {
+        private int[,] board_;
+        public Board()
+        {
+            this.board_ = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    this.board_[i, j] = 0;
+            this.pop_random();
+            this.pop_random();
+
+        }
+
+        List<Tuple<int, int>> get_empty_cells()
+        {
+            List<Tuple<int, int>> empty_cells_list = new List<Tuple<int, int>>();
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (this.board_[i, j] == 0)
+                        empty_cells_list.Add(new Tuple<int, int>(i, j));
+            return empty_cells_list;
+        }
+
+        bool pop_random()
+        {
+            Random rand = new Random();
+            List<Tuple<int, int>> empty_list = this.get_empty_cells();
+            if (empty_list.Count() == 0)
+                return false;
+            int pop_index = rand.Next(empty_list.Count()) % empty_list.Count();
+            this.board_[empty_list[pop_index].Item1, empty_list[pop_index].Item2] = 2;
+            return true;
+        }
+
+        private void merge_left()
+        {
+            int[,] new_board = this.board_;
+            int[,] moved = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            List<int[,]> list = new List<int[,]>();
+
+            //simulates up move
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 3; i >= 0; i--)
+                {
+                    if (new_board[i, j] != 0)
+                        move_left(new_board, moved, i, j);
+                }
+            }
+            this.board_ = new_board;
+        }
+
+        void merge_right()
+        {
+            int[,] new_board = this.board_;
+            int[,] moved = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            List<int[,]> list = new List<int[,]>();
+
+            //simulates up move
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (new_board[i, j] != 0)
+                        move_right(new_board, moved, i, j);
+                }
+            }
+            this.board_ = new_board;
+        }
+
+        private void move_up(int[,] board, int[,] moved, int i, int j)
+        {
+            while (j > 0)
+            {
+                //merge
+                if (moved[i, j - 1] == 0 && board[i, j - 1] == board[i, j])
+                {
+                    board[i, j - 1] = board[i, j] * 2;
+                    board[i, j] = 0;
+                    moved[i, j - 1] = 1;
+                    break;
+                }
+                //deplacement
+                else if (board[i, j - 1] == 0)
+                {
+                    board[i, j - 1] = board[i, j];
+                    board[i, j] = 0;
+                }
+                else
+                {
+                    break;
+                }
+
+                j--;
+            }
+        }
+
+        private void move_down(int[,] board, int[,] moved, int i, int j)
+        {
+            while (j < 3)
+            {
+                //merge
+                if (moved[i, j + 1] == 0 && board[i, j + 1] == board[i, j])
+                {
+                    board[i, j + 1] = board[i, j] * 2;
+                    board[i, j] = 0;
+                    moved[i, j + 1] = 1;
+                    break;
+                }
+                //deplacement
+                else if (board[i, j + 1] == 0)
+                {
+                    board[i, j + 1] = board[i, j];
+                    board[i, j] = 0;
+                }
+                else
+                {
+                    break;
+                }
+
+                j++;
+            }
+        }
+
+        private void move_left(int[,] board, int[,] moved, int i, int j)
+        {
+            while (i > 0)
+            {
+                //merge
+                if (moved[i - 1, j] == 0 && board[i - 1, j] == board[i, j])
+                {
+                    board[i - 1, j] = board[i, j] * 2;
+                    board[i, j] = 0;
+                    moved[i - 1, j] = 1;
+                    break;
+                }
+                //deplacement
+                else if (board[i - 1, j] == 0)
+                {
+                    board[i - 1, j] = board[i, j];
+                    board[i, j] = 0;
+                }
+                else
+                {
+                    break;
+                }
+
+                i--;
+            }
+        }
+
+        private void move_right(int[,] board, int[,] moved, int i, int j)
+        {
+            while (i < 3)
+            {
+                //merge
+                if (moved[i + 1, j] == 0 && board[i + 1, j] == board[i, j])
+                {
+                    board[i + 1, j] = board[i, j] * 2;
+                    board[i, j] = 0;
+                    moved[i + 1, j] = 1;
+                    break;
+                }
+                //deplacement
+                else if (board[i + 1, j] == 0)
+                {
+                    board[i + 1, j] = board[i, j];
+                    board[i, j] = 0;
+                }
+                else
+                {
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        void merge_up()
+        {
+            int[,] new_board = this.board_;
+            int[,] moved = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            List<int[,]> list = new List<int[,]>();
+
+            //simulates up move
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 3; j >= 0; j--)
+                {
+                    if (new_board[i, j] != 0)
+                        move_up(new_board, moved, i, j);
+                }
+            }
+            this.board_ = new_board;
+        }
+
+
+        void merge_down()
+        {
+            int[,] new_board = this.board_;
+            int[,] moved = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            List<int[,]> list = new List<int[,]>();
+
+            //simulates up move
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (new_board[i, j] != 0)
+                        move_down(new_board, moved, i, j);
+                }
+            }
+            this.board_ = new_board;
+        }
+
+        public void display_board()
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write(this.board_[i, j]);
+                    if (i != 3)
+                        Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void move(Keys move)
+        {
+            int[,] cpy = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    cpy[i, j] = this.board_[i, j];
+            if (move == Keys.Up)
+                merge_up();
+            else if (move == Keys.Down)
+                merge_down();
+            else if (move == Keys.Right)
+                merge_right();
+            else if (move == Keys.Left)
+                merge_left();
+            if (difference(cpy, this.board_))
+                this.pop_random();
+
+        }
+
+        private bool difference(int[,] arr1, int[,] arr2)
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (arr1[i, j] != arr2[i, j])
+                        return true;
+            return false;
+        }
+
+        private int[,] conversion(int[,] arr)
+        {
+            int[,] arrcpy = new int[4,4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    arrcpy[i, j] = arr[j, i];
+            return arrcpy;
+        }
+
+        public int[,] get_array()
+        {
+            return this.board_;
+        }
+
+    }
+}
---
+++ 2048/2048/GetBest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    public class GetBest
+    {
+        public GetBest()
+        {
+
+        }
+        /*
+                public Move_Key get_best()
+                {
+                    return this.iterative_deep();
+                }
+
+                public Move_Key iterative_deep()
+                {
+                    DateTime end = DateTime.Now.AddMilliseconds(300);
+            
+                    int depth = 0;
+                    Move_Key best;
+                    do
+                    {
+                        Move_Key newBest = this.search(depth, -10000, 10000, 0, 0);
+                        best = newBest;
+                    } while (DateTime.Now < end);
+                    return best;
+                }
+
+                /*
+                AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
+                public Move_Key search()
+                {
+                    int bestScore;
+                    Move_Key bestMove;
+                    int result;
+                    if ()
+
+                }
+
+          // the maxing player
+          if (this.grid.playerTurn) {
+            bestScore = alpha;
+            for (var direction in [0, 1, 2, 3]) {
+              var newGrid = this.grid.clone();
+              if (newGrid.move(direction).moved) {
+                positions++;
+                if (newGrid.isWin()) {
+                  return { move: direction, score: 10000, positions: positions, cutoffs: cutoffs };
+                }
+                var newAI = new AI(newGrid);
+
+                if (depth == 0) {
+                  result = { move: direction, score: newAI.eval() };
+                } else {
+                  result = newAI.search(depth-1, bestScore, beta, positions, cutoffs);
+                  if (result.score > 9900) { // win
+                    result.score--; // to slightly penalize higher depth from win
+                  }
+                  positions = result.positions;
+                  cutoffs = result.cutoffs;
+                }
+
+                if (result.score > bestScore) {
+                  bestScore = result.score;
+                  bestMove = direction;
+                }
+                if (bestScore > beta) {
+                  cutoffs++
+                  return { move: bestMove, score: beta, positions: positions, cutoffs: cutoffs };
+                }
+              }
+            }
+          }
+
+          else { // computer's turn, we'll do heavy pruning to keep the branching factor low
+            bestScore = beta;
+
+            // try a 2 and 4 in each cell and measure how annoying it is
+            // with metrics from eval
+            var candidates = [];
+            var cells = this.grid.availableCells();
+            var scores = { 2: [], 4: [] };
+            for (var value in scores) {
+              for (var i in cells) {
+                scores[value].push(null);
+                var cell = cells[i];
+                var tile = new Tile(cell, parseInt(value, 10));
+                this.grid.insertTile(tile);
+                scores[value][i] = -this.grid.smoothness() + this.grid.islands();
+                this.grid.removeTile(cell);
+              }
+            }
+
+            // now just pick out the most annoying moves
+            var maxScore = Math.max(Math.max.apply(null, scores[2]), Math.max.apply(null, scores[4]));
+            for (var value in scores) { // 2 and 4
+              for (var i=0; i<scores[value].length; i++) {
+                if (scores[value][i] == maxScore) {
+                  candidates.push( { position: cells[i], value: parseInt(value, 10) } );
+                }
+              }
+            }
+
+            // search on each candidate
+            for (var i=0; i<candidates.length; i++) {
+              var position = candidates[i].position;
+              var value = candidates[i].value;
+              var newGrid = this.grid.clone();
+              var tile = new Tile(position, value);
+              newGrid.insertTile(tile);
+              newGrid.playerTurn = true;
+              positions++;
+              newAI = new AI(newGrid);
+              result = newAI.search(depth, alpha, bestScore, positions, cutoffs);
+              positions = result.positions;
+              cutoffs = result.cutoffs;
+
+              if (result.score < bestScore) {
+                bestScore = result.score;
+              }
+              if (bestScore < alpha) {
+                cutoffs++;
+                return { move: null, score: alpha, positions: positions, cutoffs: cutoffs };
+              }
+            }
+
+          }
+
+          return { move: bestMove, score: bestScore, positions: positions, cutoffs: cutoffs };
+        }
+
+            }
+        }
+                */
+    }
+}
